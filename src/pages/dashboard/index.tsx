3b072d1f1f7d679fa77f12e667ad55fcb68be493
Michael-Bryant Choa <mbchoa@gmail.com>
2022-12-08 13:35:56 -0800

    add first iteration content to dashboard index and detail pages

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,11 +1,37 @@
 import { useRouter } from "next/router";
+import { useCallback } from "react";
 import { Layout } from "../../components/layout";
 import { LoggingIn } from "../../components/loggingIn";
 import { useRequireAuth } from "../../hooks/useRequireAuth";
+import { trpc } from "../../utils/trpc";
 
 export default function Dashboard() {
   const router = useRouter();
   const { session, loading } = useRequireAuth();
+  const { data: favoriteRestaurants } = trpc.favoriteRestaurant.all.useQuery();
+
+  const maybeRenderBody = useCallback(() => {
+    if (favoriteRestaurants === undefined) {
+      return <p>Loading...</p>;
+    }
+
+    if (favoriteRestaurants.length === 0) {
+      return <p>You have no favorite restaurants yet.</p>;
+    }
+
+    return (
+      <ul>
+        {favoriteRestaurants.map((favoriteRestaurant) => {
+          const { id, restaurant } = favoriteRestaurant;
+          return (
+            <li key={id}>
+              <p>{restaurant.name}</p>
+            </li>
+          );
+        })}
+      </ul>
+    );
+  }, [favoriteRestaurants]);
 
   if (
     loading === true ||
@@ -16,5 +42,5 @@ export default function Dashboard() {
     return <LoggingIn />;
   }
 
-  return <Layout>Dashboard stuff</Layout>;
+  return <Layout>{maybeRenderBody()}</Layout>;
 }
