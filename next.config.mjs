ed944a760679716802cdf72cbbbed213e2b48f53
Michael-Bryant Choa <mbchoa@gmail.com>
2022-12-09 21:09:13 -0800

    setup nextjs-routes

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,10 @@
  * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation.
  * This is especially useful for Docker builds.
  */
+import createNextRouteConfig from "nextjs-routes/config";
 !process.env.SKIP_ENV_VALIDATION && (await import("./src/env/server.mjs"));
+
+const withRoutes = createNextRouteConfig();
 
 /** @type {import("next").NextConfig} */
 const config = {
@@ -17,4 +20,4 @@ const config = {
     domains: ["lh3.googleusercontent.com"],
   },
 };
-export default config;
+export default withRoutes(config);
