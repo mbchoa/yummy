f753213590e777794a7e11427a57e751e581c788
Michael-Bryant Choa <mbchoa@gmail.com>
2022-12-09 18:22:59 -0800

    refactor focus/blur logic

--- src/components/searchWidget.tsx
+++ src/components/searchWidget.tsx
@@ -1,10 +1,13 @@
-import { useCallback, useEffect, useState } from "react";
+import Link from "next/link";
+import { useCallback, useEffect, useRef, useState } from "react";
+import { useClickAway } from "react-use";
 import { trpc } from "../utils/trpc";
 
 export const SearchWidget = () => {
   const [searchTerm, setSearchTerm] = useState("");
   const [location, setLocation] = useState("Los Angeles, CA");
-  const [currentFocus, setCurrentFocus] = useState<string | undefined>();
+  const [isLocationInputEnabled, setIsLocationInputEnabled] = useState(false);
+  const ref = useRef<HTMLDivElement>(null);
 
   const { data, refetch } = trpc.yelp.search.useQuery(
     {
@@ -28,24 +31,23 @@ export const SearchWidget = () => {
       data === undefined ||
       data.businesses.length === 0 ||
       searchTerm.length === 0 ||
-      location.length === 0 ||
-      (currentFocus !== "search" && currentFocus !== "location")
+      location.length === 0
     ) {
       return null;
     }
 
     return (
-      <ul className="absolute top-full left-0 right-0 bg-white p-4 drop-shadow-xl">
+      <ul className="absolute top-full left-0 right-0 space-y-4 bg-white p-4 drop-shadow-xl">
         {data.businesses.map((business) => {
           return (
             <li key={business.id}>
-              <p>{business.name}</p>
+              <Link href={`/dashboard/${business.id}`}>{business.name}</Link>
             </li>
           );
         })}
       </ul>
     );
-  }, [data, searchTerm, location, currentFocus]);
+  }, [data, searchTerm, location]);
 
   const handleSearchChange = useCallback(
     (event: React.ChangeEvent<HTMLInputElement>) => {
@@ -61,30 +63,16 @@ export const SearchWidget = () => {
     []
   );
 
-  const handleFocusChange = useCallback(
-    (event: React.FocusEvent<HTMLInputElement>) => {
-      setCurrentFocus(event.target.name);
-    },
-    []
-  );
-
-  const handleBlurChange = useCallback(() => {
-    requestAnimationFrame(() => {
-      if (
-        document !== null &&
-        !(document.activeElement instanceof HTMLInputElement)
-      ) {
-        setCurrentFocus(undefined);
-      }
-    });
+  const handleFocusChange = useCallback(() => {
+    setIsLocationInputEnabled(true);
   }, []);
 
+  useClickAway(ref, () => {
+    setIsLocationInputEnabled(false);
+  });
+
   return (
-    <div
-      className="sticky p-4"
-      onFocus={handleFocusChange}
-      onBlur={handleBlurChange}
-    >
+    <div className="sticky p-4" onFocus={handleFocusChange} ref={ref}>
       <input
         className="w-full rounded border px-3 py-2"
         value={searchTerm}
@@ -92,7 +80,7 @@ export const SearchWidget = () => {
         placeholder="In 'N Out"
         name="search"
       />
-      {(currentFocus === "search" || currentFocus === "location") && (
+      {isLocationInputEnabled && (
         <input
           className="mt-4 w-full rounded border px-3 py-2"
           value={location}
