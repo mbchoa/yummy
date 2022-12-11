0386aa132f8e6b5b6b7b3c2b83ee7b714cc3a319
Michael-Bryant Choa <mbchoa@gmail.com>
2022-12-11 00:18:55 -0800

    remove restaurant_id column

---
+++ prisma/migrations/20221211015814_remove_restaurant_id/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `restaurantId` on the `FavoriteRestaurant` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "FavoriteRestaurant" DROP COLUMN "restaurantId";
