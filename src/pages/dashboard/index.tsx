726e00d8eef043e4d6cb6b1fa7f65570fb8a8e1d
Michael-Bryant Choa <mbchoa@gmail.com>
2022-12-29 23:02:32 -0800

    add dropdown to dashboard

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -4,6 +4,7 @@ import classNames from "classnames";
 import { useSession } from "next-auth/react";
 import { useCallback, useMemo } from "react";
 import { Layout } from "../../components/layout";
+import { ListDropDown } from "../../components/modules/dashboard/listDropdown";
 import { RestaurantsList } from "../../components/modules/dashboard/restaurantsList";
 import { SearchWidget } from "../../components/searchWidget";
 import type { IYelpBusinessSchema } from "../../models/YelpSchemas";
@@ -92,24 +93,27 @@ export default function Dashboard() {
       <SearchWidget />
       <div className="px-4">
         <Tab.Group>
-          <Tab.List className="flex space-x-1 rounded-xl bg-blue-900/20 p-1">
-            {["Favorites", "Dislikes"].map((category) => (
-              <Tab
-                key={category}
-                className={({ selected }) =>
-                  classNames(
-                    "w-full rounded-lg py-2.5 text-sm font-medium leading-5 text-blue-700",
-                    "ring-white ring-opacity-60 ring-offset-2 ring-offset-blue-400 focus:outline-none focus:ring-2",
-                    selected
-                      ? "bg-white shadow"
-                      : "text-blue-100 hover:bg-white/[0.12] hover:text-white"
-                  )
-                }
-              >
-                {category}
-              </Tab>
-            ))}
-          </Tab.List>
+          <div className="flex justify-between">
+            <Tab.List className="flex basis-1/2 space-x-1 rounded-xl bg-blue-900/20 p-1">
+              {["Favorites", "Dislikes"].map((category) => (
+                <Tab
+                  key={category}
+                  className={({ selected }) =>
+                    classNames(
+                      "w-full rounded-lg py-2.5 text-sm font-medium leading-5 text-blue-700",
+                      "ring-white ring-opacity-60 ring-offset-2 ring-offset-blue-400 focus:outline-none focus:ring-2",
+                      selected
+                        ? "bg-white shadow"
+                        : "text-blue-100 hover:bg-white/[0.12] hover:text-white"
+                    )
+                  }
+                >
+                  {category}
+                </Tab>
+              ))}
+            </Tab.List>
+            <ListDropDown />
+          </div>
           <Tab.Panels className="mt-4">{maybeRenderPanels()}</Tab.Panels>
         </Tab.Group>
       </div>
