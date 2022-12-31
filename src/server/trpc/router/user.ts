2ae5b4fd99c11e39202e6984bc51b1b992c33c20
Michael-Bryant Choa <mbchoa@gmail.com>
2022-12-30 22:42:45 -0800

    add search user endpoint

--- src/server/trpc/router/user.ts
+++ src/server/trpc/router/user.ts
@@ -1,7 +1,19 @@
+import { z } from "zod";
 import { protectedProcedure, router } from "../trpc";
 
 export const user = router({
   all: protectedProcedure.query(({ ctx }) => {
     return ctx.prisma.user.findMany();
   }),
+  search: protectedProcedure
+    .input(
+      z.object({
+        term: z.string(),
+      })
+    )
+    .query(({ input, ctx }) => {
+      const searchTerm = `${input.term}:*`;
+      return ctx.prisma
+        .$queryRaw`SELECT * FROM "public"."User" WHERE to_tsvector(concat_ws(' ', "public"."User"."email","public"."User"."name")) @@ to_tsquery(${searchTerm})`;
+    }),
 });
