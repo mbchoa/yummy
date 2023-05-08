7b39307f21f81f43b07123a76049a1294f53f086
Michael-Bryant Choa <mbchoa@gmail.com>
2023-05-08 00:12:46 -0700

    test caching

--- src/pages/api/trpc/[trpc].ts
+++ src/pages/api/trpc/[trpc].ts
@@ -14,4 +14,24 @@ export default createNextApiHandler({
           console.error(`❌ tRPC failed on ${path}: ${error}`);
         }
       : undefined,
+  responseMeta(opts) {
+    debugger;
+    const { paths, errors, type } = opts;
+    // assuming you have all your public routes with the keyword `public` in them
+    const allPublic = paths && paths.every((path) => path.includes("yelp"));
+    // checking that no procedures errored
+    const allOk = errors.length === 0;
+    // checking we're doing a query request
+    const isQuery = type === "query";
+    if (allPublic && allOk && isQuery) {
+      // cache request for 1 day + revalidate once every second
+      const ONE_DAY_IN_SECONDS = 60 * 60 * 24;
+      return {
+        headers: {
+          "cache-control": `s-maxage=1, stale-while-revalidate=${ONE_DAY_IN_SECONDS}`,
+        },
+      };
+    }
+    return {};
+  },
 });
